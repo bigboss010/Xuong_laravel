367878e7500b40b5d3bde6ea6230bb2c49729731
bao141004 <vuvanbao347@gmail.com>
2024-07-13 20:37:56 +0700

    cat layout

---
+++ resources/views/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Dashboard</title>
+    @include('layouts.style')
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+        @include('layouts.sidebar')
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                @include('layouts.nav')
+                <div class="container-fluid">
+                    <div class="row">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+            @include('layouts.footer')
+        </div>
+    </div>
+    @include('layouts.script')
+</body>
+</html>
