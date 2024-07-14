2bfc26aec464884ffe2069beaef8002659e5ef6c
bao141004 <vuvanbao347@gmail.com>
2024-07-14 15:42:16 +0700

    done users + chuc vu

---
+++ app/Models/KhachHang.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class KhachHang extends Model
+{
+    use HasFactory;
+
+    public function getList()
+    {
+        $listUsers = DB::table('users')
+            ->join('chuc_vus', 'users.chuc_vu_id', '=', 'chuc_vus.id')
+            ->select('users.*', 'chuc_vus.ten_chuc_vu')
+            ->orderBy('users.id')
+            ->get();
+        return $listUsers;
+    }
+
+    public function createUser($data){
+        DB::table('users')->insert($data);
+    }
+}
