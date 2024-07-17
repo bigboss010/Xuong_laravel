eeea94438b370bb9b7a51ed39c872f032a5ea52d
bao141004 <vuvanbao347@gmail.com>
2024-07-17 14:57:02 +0700

    trang thai don hang done

--- resources/views/admins/trangthaidonhang/index.blade.php
+++ resources/views/admins/trangthaidonhang/index.blade.php
@@ -1,7 +1,6 @@
 @extends('master')
 
 @section('content')
-
     <div class="d-sm-flex align-items-center justify-content-between mb-3">
         <div>
             <h1 class="h3 text-gray-800">{{ $title }}</h1>
@@ -14,31 +13,39 @@
                 {{ session('success') }}
             </div>
         @endif
+        @if (session('errors'))
+        <div class="alert alert-danger">
+            {{ session('errors') }}
+        </div>
+    @endif
     </div><br>
-  
-        <table class="table">
-            <thead>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Tên Trạng Thái</th>
+                <th scope="col">Hành Động</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($list as $index => $value)
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Tên Trạng Thái</th>
-                    <th scope="col">Hành Động</th>
+                    <td>{{ $index + 1 }} </td>
+                    <td>{{ $value->ten_trang_thai }} </td>
+
+
+                    <td>
+                        <a href="{{ route('trang_thai_don_hangs.edit', $value->id) }}"><button type="button"
+                                class="btn btn-warning">Sửa</button></a>
+                        <form action="{{ route('trang_thai_don_hangs.destroy', $value->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <a href=""><button type="submit" class="btn btn-danger" onclick="return confirm('Bạn chắc chắn muốn xóa ?')">Xóa</button></a>
+                        </form>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach ($list as $index=>$value)
-                
-                    <tr>
-                        <td>{{$index +1}}  </td>
-                        <td>{{$value->ten_trang_thai}}  </td>
-                        
-                 
-                        <td>
-                            <a href=""><button type="button"
-                                    class="btn btn-warning">Sửa</button></a>
-                        </td>
-                    </tr>
-                @endforeach 
-            </tbody>
-        </table>
-   
+            @endforeach
+        </tbody>
+    </table>
 @endsection
